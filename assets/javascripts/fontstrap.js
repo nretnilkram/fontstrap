ff325c0532b2dd8736b0292e6d6ea16f41d687f2
Mark Lintern <mark.lintern@oracle.com>
2016-12-06 15:06:10 -0500

    updated for single js file

---
+++ assets/javascripts/fontstrap.js
@@ -0,0 +1,30 @@
+function loadScript(url, callback){
+
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+
+    if (script.readyState){  //IE
+        script.onreadystatechange = function(){
+            if (script.readyState == "loaded" ||
+                    script.readyState == "complete"){
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function(){
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+loadScript("dist/js/jquery-3.1.1.min.js", function(){ 
+    loadScript("dist/js/tether.min.js", function(){ 
+        loadScript("dist/js/bootstrap.min.js", function(){
+            $(function () {$('[data-toggle="tooltip"]').tooltip();});
+        });
+    });
+});
